ad4a65c485109f7332fb616d885b3f865a7c45de
Nhu <nhuchnce181233@fpt.edu.vn>
2026-03-04 23:52:31 +0700

    [Iter5][UC-53] fix: Integrate WebSocket for Create Transfer Request

--- backendAPI/src/modules/transfers/transfers.module.ts
+++ backendAPI/src/modules/transfers/transfers.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { TransfersController } from './transfers.controller';
 import { TransfersService } from './transfers.service';
 import { Transfer, TransferSchema } from './transfers.schema';
+import { GatewaysModule } from '../../common/gateways/gateways.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Transfer.name, schema: TransferSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Transfer.name, schema: TransferSchema }]),
+    GatewaysModule,
+  ],
   controllers: [TransfersController],
   providers: [TransfersService],
 })
--- backendAPI/src/modules/transfers/transfers.service.ts
+++ backendAPI/src/modules/transfers/transfers.service.ts
@@ -1,19 +1,25 @@
+
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Transfer } from './transfers.schema';
 import { CreateTransferDto } from './dto/create-transfer.dto';
+import { EventsGateway } from '../../common/gateways/events.gateway';
 
 @Injectable()
 export class TransfersService {
   constructor(
     @InjectModel(Transfer.name) private transferModel: Model<Transfer>,
+    private readonly eventsGateway: EventsGateway,
   ) {}
 
   async create(createTransferDto: CreateTransferDto): Promise<Transfer> {
     const created = new this.transferModel({
       ...createTransferDto
     });
-    return created.save();
+    const result = await created.save();
+    // Emit websocket event
+    this.eventsGateway.server.emit('transfer:created', result);
+    return result;
   }
 }
